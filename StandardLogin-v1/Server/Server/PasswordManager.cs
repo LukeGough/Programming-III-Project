65468744fb2467f41fcb87bd97a725471579848d
LukeGough <37359964+LukeGough@users.noreply.github.com>
2019-11-29 16:51:02 +0800

    Add files via upload

---
+++ StandardLogin-v1/Server/Server/PasswordManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class PasswordManager
+    {
+        HashComputer m_HashComputer = new HashComputer();
+
+        public string GeneratePasswordHash(string plainTextPassword, out string salt)
+        {
+            salt = SaltGenerator.GetSaltString();
+
+            string finalString = plainTextPassword + salt;
+
+            return m_HashComputer.GetPasswordHashAndSalt(finalString);
+        }
+
+        public bool IsPasswordMatch(string password, string salt, string hash)
+        {
+            string finalString = password + salt;
+            return hash == m_HashComputer.GetPasswordHashAndSalt(finalString);
+        }
+    }
+}
