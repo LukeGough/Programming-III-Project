9670355b52f3f0575c8a9eda6ab9bdc3d2e42476
LukeGough <37359964+LukeGough@users.noreply.github.com>
2019-12-01 10:35:24 +0800

    Add files via upload

---
+++ StandardLogin-v3/Client/Client/ClientForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Client
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbUserId = new System.Windows.Forms.TextBox();
+            this.tbPipeName = new System.Windows.Forms.TextBox();
+            this.lbPipeName = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lbUserId = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.tbReceived = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(71, 97);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(160, 20);
+            this.tbPassword.TabIndex = 3;
+            // 
+            // tbUserId
+            // 
+            this.tbUserId.Location = new System.Drawing.Point(71, 71);
+            this.tbUserId.Name = "tbUserId";
+            this.tbUserId.Size = new System.Drawing.Size(160, 20);
+            this.tbUserId.TabIndex = 2;
+            // 
+            // tbPipeName
+            // 
+            this.tbPipeName.Location = new System.Drawing.Point(12, 25);
+            this.tbPipeName.Name = "tbPipeName";
+            this.tbPipeName.ReadOnly = true;
+            this.tbPipeName.Size = new System.Drawing.Size(219, 20);
+            this.tbPipeName.TabIndex = 0;
+            this.tbPipeName.Text = "\\\\.\\pipe\\myNamedPipe";
+            // 
+            // lbPipeName
+            // 
+            this.lbPipeName.AutoSize = true;
+            this.lbPipeName.Location = new System.Drawing.Point(12, 9);
+            this.lbPipeName.Name = "lbPipeName";
+            this.lbPipeName.Size = new System.Drawing.Size(62, 13);
+            this.lbPipeName.TabIndex = 0;
+            this.lbPipeName.Text = "Pipe Name:";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(237, 25);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 20);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(151, 123);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(80, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lbUserId
+            // 
+            this.lbUserId.AutoSize = true;
+            this.lbUserId.Location = new System.Drawing.Point(22, 74);
+            this.lbUserId.Name = "lbUserId";
+            this.lbUserId.Size = new System.Drawing.Size(43, 13);
+            this.lbUserId.TabIndex = 0;
+            this.lbUserId.Text = "User ID";
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(12, 100);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(53, 13);
+            this.lbPassword.TabIndex = 0;
+            this.lbPassword.Text = "Password";
+            // 
+            // tbReceived
+            // 
+            this.tbReceived.Location = new System.Drawing.Point(12, 152);
+            this.tbReceived.Multiline = true;
+            this.tbReceived.Name = "tbReceived";
+            this.tbReceived.ReadOnly = true;
+            this.tbReceived.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbReceived.Size = new System.Drawing.Size(300, 138);
+            this.tbReceived.TabIndex = 0;
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 295);
+            this.Controls.Add(this.tbReceived);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbUserId);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.lbPipeName);
+            this.Controls.Add(this.tbPipeName);
+            this.Controls.Add(this.tbUserId);
+            this.Controls.Add(this.tbPassword);
+            this.Name = "ClientForm";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbUserId;
+        private System.Windows.Forms.TextBox tbPipeName;
+        private System.Windows.Forms.Label lbPipeName;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lbUserId;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.TextBox tbReceived;
+    }
+}
+
